c35be8789b57085eaf68848c7768718b06a9fc37
Nikeguga <nikeguga@gmail.com>
2023-07-04 22:47:50 +0300

    homework 1 done

---
+++ homework_1/Program.cs
@@ -0,0 +1,75 @@
+﻿// //Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт большее число
+
+// Console.Write ("Input first number: ");
+// double num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write ("Input second number: ");
+// double num2 = Convert.ToInt32(Console.ReadLine());
+
+// if(num1 > num2)
+// {
+//     Console.WriteLine(num1);
+// }
+// if(num1 < num2)
+// {
+//     Console.WriteLine(num2);
+// }
+// else
+// {
+
+//     Console.WriteLine("You do like this number to put it twice, do you?");
+// }
+
+// //Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.WriteLine ("Input first number: ");
+// double num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Input second number: ");
+// double num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Input first number: ");
+// double num3 = Convert.ToInt32(Console.ReadLine());
+
+// double max = num1;
+
+// if (num2 > max)
+// {
+//     max = num2;
+// }
+// if (num3 > max)
+// {
+//     max = num3;
+// }
+// Console.WriteLine(max);
+
+// //Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// Console.WriteLine ("Input a number: ");
+// double num = Convert.ToInt32(Console.ReadLine());
+// if(num % 2 == 0)
+// {
+//     Console.WriteLine(num + " is even");
+// }
+// else
+// {
+//     Console.WriteLine(num + " is odd");
+// }
+
+// /*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// пойдем долгим путём для практики, т.к. в целом, можно взять за старт сразу 2 и дальше делать шаг +=2 и сразу вывести с учётом ограничения*/
+// Console.WriteLine ("Input a number: ");
+// double num = Convert.ToInt32(Console.ReadLine());
+
+// double current = 1;
+
+// while (current <= num) // т.к. в примере ввод = 8 и 8 в выдаче включаем число из ввода в проверяемые под вывод
+// {
+//     if (current % 2 == 0)
+//     {
+//         Console.Write(current + " ");
+//         current += 1;
+//     }
+//     else
+//     {
+//         current +=1;
+//     }
+// }
+
--- seminar_1/Program.cs
+++ seminar_1/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 45;
+double b = 36.6;
+string c = "blow my mind";
+bool d = false;
+// we comment here or we can go ctrl + /, which will allow you to highlight anything you wish and convert it to comment or use /* as start and */ at the end
+int number =5;
+int number2 =7;
+// contetination goes with + in Console.WriteLine
+Console.WriteLine("My nymbers are " + number + " and " + number2 + " they are the beeeest!");
+// contatination may be performed with $ and getting veriables in {}
+Console.WriteLine($"My numbers are {number} and {number2} they are the beeest!");
+
+// request from user + convert input to proper type of data
+Console.Write ("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Your number, that you've just entered is " + num);
+// is number a square of another?
+Console.Write ("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int result = num2 * num2;
+if(num1 == result)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
+// bese cycl. we get inputs and then we get all numbers in range
+Console.WriteLine("Input your so positive numbre: ");
+int numbre = Convert.ToInt32(Console.ReadLine());
+int current = -numbre;
+
+while(current <= numbre)
+{
+    Console.Write(current + " ");
+    current += 1;
+}
+
+// homework via link on github
